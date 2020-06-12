436a2f6bf3aac9d1e3991cff018196e0909ae5d3
Michal <mkolodziejczyk269@gmail.com>
2020-06-12 18:46:12 +0200

    Create Navbar

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav
+      className="navbar  navbar-dark bg-light"
+      style={{ backgroundColor: "#e3f2fd" }}
+    >
+      <ul className="nav navbar_nav container">
+        <li className="nav-item ">
+          <a className="nav-link text-dark" href="#">
+            Odzież
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-dark" href="#">
+            Buty
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-dark" href="#">
+            Plecaki
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-dark" href="#">
+            Wspinanie
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-dark" href="#">
+            Turytyka
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../layout/Header";
+import Navbar from "../layout/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <Navbar />
     </div>
   );
 };
