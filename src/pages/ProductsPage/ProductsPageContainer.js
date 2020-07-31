6fced4164fdb36dbbbcb7c0182284e34c06bf130
Michal <mkolodziejczyk269@gmail.com>
2020-07-31 10:29:48 +0200

    Create ProductsPage

---
+++ src/pages/ProductsPage/ProductsPageContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ProductsPageComponent from "./component/ProductsPageComponent";
+
+const ProductsPageContainer = () => {
+  return <ProductsPageComponent />;
+};
+
+export default ProductsPageContainer;
---
+++ src/pages/ProductsPage/component/ProductsPageComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductsPageComponent = () => {
+  return <div>Hello</div>;
+};
+
+export default ProductsPageComponent;
