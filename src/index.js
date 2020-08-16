f4e87c7df67c97c86b4a516ff9ed63e2fdf1ea6e
Michal <mkolodziejczyk269@gmail.com>
2020-08-16 18:58:15 +0200

    Add AWS s3 configure

--- src/index.js
+++ src/index.js
@@ -13,6 +13,12 @@ Amplify.configure({
     region: config.cognito.REGION,
     userPoolId: config.cognito.USER_POOL_ID,
     userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  Storage: {
+    AWSS3: {
+      bucket: config.bucket.BUCKET,
+      region: config.bucket.REGION
+    }
   }
 });
 
