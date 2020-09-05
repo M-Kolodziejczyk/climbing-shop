bd1f5897f7fc27452d7d8251b785f8219e79e89b
Michal <mkolodziejczyk269@gmail.com>
2020-09-05 16:11:10 +0200

    Set up Navbar link

--- src/common/components/Navbar.js
+++ src/common/components/Navbar.js
@@ -8,28 +8,23 @@ const Navbar = () => {
     >
       <ul className="nav navbar_nav container">
         <li className="nav-item ">
-          <a className="nav-link text-dark" href="#!">
-            Odzież
+          <a className="nav-link text-dark" href="/products/category/clothing">
+            Clothing
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link text-dark" href="#!">
-            Buty
+          <a className="nav-link text-dark" href="/products/category/shoes">
+            Shoes
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link text-dark" href="#!">
-            Plecaki
+          <a className="nav-link text-dark" href="/products/category/backpacks">
+            Backpacks
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link text-dark" href="#!">
-            Wspinanie
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link text-dark" href="#!">
-            Turytyka
+          <a className="nav-link text-dark" href="/products/category/climbing">
+            Climbing
           </a>
         </li>
       </ul>
