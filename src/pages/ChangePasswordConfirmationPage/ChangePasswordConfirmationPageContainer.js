db46f1595c7c3d3628a8182f2d2df9ed70ba69f5
Michal <mkolodziejczyk269@gmail.com>
2020-09-15 15:45:28 +0200

    add redirect to ChangePasswordConfirmationPage

--- src/pages/ChangePasswordConfirmationPage/ChangePasswordConfirmationPageContainer.js
+++ src/pages/ChangePasswordConfirmationPage/ChangePasswordConfirmationPageContainer.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import ChangePasswordConfirmationPageComponent from "./components/ChangePasswordConfirmationPageComponent";
 
 const ChangePasswordConfirmationPageContainer = () => {
+  let history = useHistory();
+  const changePassword = useSelector(state => state.auth.changePassword);
+
+  if (!changePassword) {
+    history.push("/");
+  }
   return <ChangePasswordConfirmationPageComponent />;
 };
 
